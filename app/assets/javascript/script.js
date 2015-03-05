54546dba8e613bc2c86a471838d27da4dd2fa5a9
santoshsankar <santoshsankar@gmail.com>
2015-03-05 11:51:21 -0500

    first commit

---
+++ app/assets/javascript/script.js
@@ -0,0 +1,12 @@
+var main = function(){
+
+	$('#menu > ul.nav li a').click(function(e) {
+    	var $this = $(this);
+    	$this.parent().siblings().removeClass('active').end().addClass('active');
+    	
+	});
+
+
+};
+
+$(document).ready(main);
